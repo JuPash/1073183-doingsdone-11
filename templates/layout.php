d865226b9030116db78ad8ef6f7eb56044be9d69
Юля <volage789@gmail.com>
2019-12-22 15:55:36 +0300

    фильтрация на выводе имени пользователя

--- templates/layout.php
+++ templates/layout.php
@@ -23,7 +23,7 @@
                 <div class="main-header__side-item user-menu">
                     <div class="user-menu__data">
                         <p>
-                            <?= $user_info['name'] ?>
+                            <?= filterXSS($user_info['name']) ?>
                         </p>
                         <a href="/logout.php">Выйти</a>
                     </div>
